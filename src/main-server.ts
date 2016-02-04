ba8a33bda8c39e0904e38e58c8111cae3a273c71
Jason Teplitz <jteplitz602@gmail.com>
2016-02-03 21:28:15 -0800

    chore: Upgrade angular to beta.3 and universal to 0.5.0

--- src/main-server.ts
+++ src/main-server.ts
@@ -2,7 +2,11 @@
 
 import * as path from 'path';
 import * as express from 'express';
-import {SERVER_LOCATION_PROVIDERS, ng2engine} from 'angular2-universal-preview/dist/server';
+import {
+  SERVER_LOCATION_PROVIDERS,
+  ng2engine,
+  REQUEST_URL
+} from 'angular2-universal-preview/dist/server';
 
 import {provide} from 'angular2/core';
 import {APP_BASE_HREF, ROUTER_PROVIDERS} from 'angular2/router';
@@ -28,6 +32,7 @@ app.use('/', (req, res) => {
   res.render('index', { App, providers: [
     ROUTER_PROVIDERS,
     SERVER_LOCATION_PROVIDERS,
+    provide(REQUEST_URL, {useValue: req.originalUrl}),
     provide(APP_BASE_HREF, {useValue: `http://localhost:3000${req.baseUrl}`}),
     SHARED_PROVIDERS
   ] });
