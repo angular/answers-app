2dfd65fa8160b74e216726a282f8a57bd941c541
Jeff Cross <middlefloor@gmail.com>
2016-02-12 15:23:07 -0800

    chore(typings): add temporary hack to work around universal typings issue

---
+++ temporary-render.d.ts
@@ -0,0 +1,7 @@
+/**
+ * This file gets copied into the universal node_modules
+ * directory during postinstall to work around an issue
+ * with server/src/render.d.ts not being present or considered
+ * a module.
+ */
+export declare var thisIsOnlyTemporary:string;
