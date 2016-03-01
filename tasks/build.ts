8df242159c56f1ab3cba6ffc0217138e0cccd1a0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-03-01 15:49:27 -0800

    feat(build): add service worker generation via sw-precache

--- tasks/build.ts
+++ tasks/build.ts
@@ -3,10 +3,14 @@ declare var require;
 const sass = require('gulp-sass');
 const inline = require('gulp-inline');
 const minifyCSS = require('gulp-minify-css');
+const path = require('path');
+const swPrecache = require('sw-precache');
 
 export const build = (gulp, config) => {
+  
+  gulp.task('default', ['build', 'build:sw-precache']);
 
-	gulp.task('default', ['compile:sass','compile:app','copy:dev'], () => {
+	gulp.task('build', ['compile:sass','compile:app','copy:dev'], () => {
 
 		gulp.src(config.index)
   		.pipe(inline({
@@ -17,6 +21,19 @@ export const build = (gulp, config) => {
   		.pipe(gulp.dest('dist/'));
 
 	});
+  
+  gulp.task('build:sw-precache', ['build'], (done) => {
+    swPrecache.write(
+      'dist/service-worker.js',
+      {
+        staticFileGlobs: [
+          'dist/**/*.{css,html,jpg,js,png,svg}',
+          '!dist/service-worker.js'
+        ],
+        stripPrefix: 'dist'
+      },
+      done);
+  });
 
 
 }
